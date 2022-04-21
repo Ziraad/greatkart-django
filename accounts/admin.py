66c167d573cd2a11e30958c211301e255d58ad28
Ziraad <parand8631@gmail.com>
2022-04-21 17:47:04 +0430

    django authentiacation

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,13 +4,13 @@ from .models import Account
 
 # Register your models here.
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('date_joined', 'last_login')
-    ordering = ('date_joined',)
+    ordering = ('-date_joined',)
 
     filter_horizontal = ()
     list_filter = ()
-    filtersets = ()
+    fieldsets = ()
 
 admin.site.register(Account, AccountAdmin)
